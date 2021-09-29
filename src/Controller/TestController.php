7a5c748367928d0c7190f5f4329531b578413ec0
Jean-Paul BELLA <jean.paul.bella@hotmail.fr>
2021-09-29 07:19:41 +0200

    After renaming

---
+++ src/Controller/TestController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'test')]
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+
+
+    public function publish(HubInterface $hub): Response
+    {
+        $update = new Update(
+            '/books/1',
+            json_encode(['status' => 'OutOfStock'])
+        );
+        $hub->publish($update);
+
+        return $this->json(["message" => "published"]);
+    }
+}
