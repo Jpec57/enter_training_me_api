6253f5f8f9253e43760b4b44a56548bc81aaa0c3
Jean-Paul BELLA <jean.paul.bella@hotmail.fr>
2021-10-02 22:45:30 +0200

    test

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -7,12 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
+
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'test')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
+        return $this->json([
             'controller_name' => 'TestController',
         ]);
     }
