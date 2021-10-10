dd77db7a11d37b103696d60ac332c582795537a3
Jpec57 <jean.paul.bella@hotmail.fr>
2021-10-10 12:25:11 +0200

    help

--- src/Entity/Training.php
+++ src/Entity/Training.php
@@ -14,11 +14,11 @@ use Symfony\Component\Serializer\Annotation\Groups;
  * @ORM\Entity(repositoryClass=TrainingRepository::class)
  */
 #[ApiResource(
+    collectionOperations: ['post' => [
+        'denormalization_context' => ['groups' => ['default', 'realised_exercise_set', 'realised_exercise_exercise_reference', 'exercise_cycle_exercise', 'training_exercise_cycle', 'training_user', 'exercise_cycle_exercise']],
+    ]],
     itemOperations: [
         "get" => [
-            'denormalization_context' => ['groups' => ['default', 'realised_exercise_set', 'realised_exercise_exercise_reference', 'exercise_cycle_exercise', 'training_exercise_cycle', 'training_user', 'exercise_cycle_exercise']],
-        ],
-        "post" => [
             'denormalization_context' => ['groups' => ['default', 'realised_exercise_set', 'realised_exercise_exercise_reference', 'exercise_cycle_exercise', 'training_exercise_cycle', 'training_user', 'exercise_cycle_exercise']],
         ],
         "patch" => [
